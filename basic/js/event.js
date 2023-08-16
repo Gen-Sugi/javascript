89b3228bbad812359004f227a4e6085ab9984eed
Gen-Sugi <gsnoyabou1230@gmail.com>
2023-08-16 18:16:17 +0900

    js_18章　完了

--- basic/js/event.js
+++ basic/js/event.js
@@ -1,21 +1,47 @@
-  // output-btnというidを持つHTML要素を取得し、定数に代入する
-  const btn = document.getElementById('output-btn');
- 
-  // HTML要素がクリックされたときにイベント処理を実行する
-  btn.addEventListener('click', () => {
-    console.log('クリックされました！');
-  });
+// output-btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById("output-btn");
 
-    // add-btnというidを持つHTML要素を取得し、定数に代入する
-    const addBtn = document.getElementById("add-btn");
-    // parent-listというidを持つHTML要素を取得し、定数に代入する
-    const parentList = document.getElementById("parent-list");
+// HTML要素がクリックされたときにイベント処理を実行する
+btn.addEventListener("click", () => {
+  console.log("クリックされました！");
+});
 
-    addBtn.addEventListener("click",  () => {
-      const childList = document.createElement("li");
-      childList.textContent = "これはリスト要素です";
-      parentList.appendChild(childList);
-    });
+// add-btnというidを持つHTML要素を取得し、定数に代入する
+const addBtn = document.getElementById("add-btn");
+// parent-listというidを持つHTML要素を取得し、定数に代入する
+const parentList = document.getElementById("parent-list");
 
- 
-  
+addBtn.addEventListener("click", () => {
+  const childList = document.createElement("li");
+  childList.textContent = "これはリスト要素です";
+  parentList.appendChild(childList);
+});
+
+// ↓↓↓↓↓↓　文字数カウント　↓↓↓↓↓↓
+const countBtn = document.getElementById("count-btn");
+
+countBtn.addEventListener("click", () => {
+  const text = document.forms.textForm.textBox.value;
+  console.log(text.length + "文字");
+});
+
+// ↓↓↓↓↓↓　ラジオボタンの値獲得　↓↓↓↓↓↓
+const areaBtn = document.getElementById("area-btn");
+
+areaBtn.addEventListener("click", () => {
+  const radioValue = document.forms.areaForm.area.value;
+  console.log(radioValue);
+});
+
+const osBtn = document.getElementById("os-btn");
+
+// チャックボックス
+osBtn.addEventListener("click", () => {
+  const osNames = document.forms.osForm.os;
+
+  for (let i = 0; i < osNames.length; i++) {
+    if (osNames[i].checked) {
+      console.log(osNames[i].value);
+    }
+  }
+});
